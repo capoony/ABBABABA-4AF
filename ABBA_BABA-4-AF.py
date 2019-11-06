b39b366bdc84b3e5b643f6e769c20277db7a3c2a
MartinKapun <capoony@gmail.com>
2019-11-06 11:34:37 +0100

    Public Beta

--- ABBA_BABA-4-AF.py
+++ ABBA_BABA-4-AF.py
@@ -19,11 +19,9 @@ group=OptionGroup(parser,'''
 
 #########################################################   CODE   #########################################################################
 
-parser.add_option("--AlleleFrequencies", dest="AF", help="Input file")
-parser.add_option("--output", dest="OUT", help="Output file")
-parser.add_option("--order", dest="ORDER", help="column positions of populations H1,H2,H3,H4")
-parser.add_option("--window", dest="W", help="window-size")
-parser.add_option("--step", dest="S", help="step-size")
+parser.add_option("--AlleleFrequencies", dest="AF", help="Input file; First three columns: Chrom, Pos, MajorAllele/MinorAllele")
+parser.add_option("--output", dest="OUT", help="Output prefix")
+parser.add_option("--order", dest="ORDER", help="column positions of populations H1,H2,H3,H4 in input file")
 parser.add_option("--SNPs", dest="SNP", help="number of SNPS (overrules window and stepsize)",default="NA")
 
 (options, args) = parser.parse_args()
@@ -50,22 +48,11 @@ def D2(x,XL,YL,fXL,fYL,N):
     fYL[N].append(Y)
 
 
-def D3(x,XL,YL,fXL,fYL,N):
-    '''calculate nominator X and demoninator Y a la Martin et al 2014, assumes X=ABBA-BABA'''
-    A,B,C,D=x
-    if C>B:
-        A,C,B,D=x
-    X=(1-A)*B*C*(1-D)-A*(1-B)*C*(1-D)
-    Y=(1-A)*B*B*(1-D)-A*(1-B)*B*(1-D)
-    XL.append(X)
-    fXL[N].append(X)
-    YL.append(Y)
-    fYL[N].append(Y)
 
 def blockJackEven(fx,fy,D):
     ''' see Busing et al. 1999 '''
 
-    ## at first retain windows that don't result in window-wise D="NA"
+    ## at first retain only windows that don't result in window-wise D="NA"
     fxnew=[]
     fynew=[]
     for i in range(len(D)):
@@ -90,15 +77,16 @@ def blockJackEven(fx,fy,D):
     ## caluclate the mean of all group-wise estimates of D
     meanJack=sum(Dmean)/float(len(Dmean))
 
-    ## calculate the jackknife estimate and variance of D
+    ## calculate the jackknife estimate and variance of D and z-score
     DjackEst=g*Dest-(g-1)*meanJack
     DjackVar=((g-1)/g)*sum([(Di-Dest)**2 for Di in Dmean])
+    Z=Dest/(DjackVar**0.5)
 
     ## return
-    return Dest,DjackEst,DjackVar,Dest/(DjackVar**0.5)
+    return Dest,DjackEst,DjackVar,Z
 
 def test0(x,y,z):
-    ''' calculate the ratio of x and y if enough SNPs and if y is not 0, then append to list z '''
+    ''' calculate the ratio of x and y if bin contains enough SNPs and if y is not 0, then append to list z '''
 
     if sum(y)==0 or len(x)!=SNPs:
         xy="NA"
@@ -123,7 +111,7 @@ def load_data(x):
 
 
 SNPs=int(options.SNP)
-ORDER=[*map(int,options.ORDER.split(","))]
+ORDER=[int(x) for x in options.ORDER.split(",")]
 
 Chr=""
 
@@ -131,18 +119,24 @@ Chr=""
 SNPwindows=[]
 
 ## to calculate a global value
-fSNPX1,fSNPX2,fSNPX3=d(list),d(list),d(list)
-fSNPY1,fSNPY2,fSNPY3=d(list),d(list),d(list)
+fSNPX1,fSNPX2=d(list),d(list)
+fSNPY1,fSNPY2=d(list),d(list)
 
 ## to calculate window-wise values
-SNPX1,SNPX2,SNPX3=[],[],[]
-SNPY1,SNPY2,SNPY3=[],[],[]
+SNPX1,SNPX2=[],[]
+SNPY1,SNPY2=[],[]
 
 ## make list to store window-wise values of D
-D1L,D2L,D3L=[],[],[]
+D1L,D2L=[],[]
 
-out=open(options.OUT,"w")
+## open Outputfile for window-wise estimates
+outWindow=open(options.OUT+"_"+str(SNPs)+"SNPs.D","w")
+
+## Counter of SNP groups
 N=0
+
+outWindow.write("Chromosome\tStartPos\tAveragePos\tEndPos\tWindowSize\tNoOfSNPs\tSoraggi\tDurand\n")
+
 for l in load_data(options.AF):
     a=l.rstrip().split()
 
@@ -155,8 +149,9 @@ for l in load_data(options.AF):
     if "NA" in AFs or "na" in AFs:
         continue
 
-    AFs=[*map(float,AFs)]
+    AFs=[float(x) for x in AFs]
 
+    ## continue if site is not informative according to Sorragi et al., i.e. if H1==H2 or H3==H4
     if AFs[0]==AFs[1] or AFs[2]==AFs[3]:
         continue
 
@@ -172,7 +167,6 @@ for l in load_data(options.AF):
         # calculate D
         D1s=test0(SNPX1,SNPY1,D1L)
         D2s=test0(SNPX2,SNPY2,D2L)
-        D3s=test0(SNPX3,SNPY3,D3L)
 
         if len(SNPX1)!=0:
             START=min(SNPwindows)
@@ -181,20 +175,19 @@ for l in load_data(options.AF):
             LENGTH=END-START
 
             # only print the values of D if the number of SNPs is equivalent to the number of SNPs defined as the binsize
-            out.write(Chr+"\t"+"\t".join([*map(str,[START,MIDDLE,END,LENGTH])])+"\t"+str(len(SNPX1))+"\t"+D1s+"\t"+D2s+"\t"+D3s+"\n")
+            outWindow.write(Chr+"\t"+"\t".join([str(x) for x in [START,MIDDLE,END,LENGTH]])+"\t"+str(len(SNPX1))+"\t"+D1s+"\t"+D2s+"\n")
 
         # reset the lists
         SNPwindows=[]
-        SNPX1,SNPX2,SNPX3=[],[],[]
-        SNPY1,SNPY2,SNPY3=[],[],[]
+        SNPX1,SNPX2=[],[]
+        SNPY1,SNPY2=[],[]
         Chr=a[0]
 
         SNPwindows.append(int(a[1]))
 
         # store the numerator and denominator of D
-        D1([*map(float,AFs)],SNPX1,SNPY1,fSNPX1,fSNPY1,N)
-        D2([*map(float,AFs)],SNPX2,SNPY2,fSNPX2,fSNPY2,N)
-        D3([*map(float,AFs)],SNPX3,SNPY3,fSNPX3,fSNPY3,N)
+        D1([float(x) for x in AFs],SNPX1,SNPY1,fSNPX1,fSNPY1,N)
+        D2([float(x) for x in AFs],SNPX2,SNPY2,fSNPX2,fSNPY2,N)
 
         ## increase counter by one to start a new group
         N+=1
@@ -204,13 +197,11 @@ for l in load_data(options.AF):
     SNPwindows.append(int(a[1]))
     Chr=a[0]
 
-    D1([*map(float,AFs)],SNPX1,SNPY1,fSNPX1,fSNPY1,N)
-    D2([*map(float,AFs)],SNPX2,SNPY2,fSNPX2,fSNPY2,N)
-    D3([*map(float,AFs)],SNPX3,SNPY3,fSNPX3,fSNPY3,N)
+    D1([float(x) for x in AFs],SNPX1,SNPY1,fSNPX1,fSNPY1,N)
+    D2([float(x) for x in AFs],SNPX2,SNPY2,fSNPX2,fSNPY2,N)
 
 D1s=test0(SNPX1,SNPY1,D1L)
 D2s=test0(SNPX2,SNPY2,D2L)
-D3s=test0(SNPX3,SNPY3,D3L)
 
 if len(SNPX1)!=0:
 
@@ -219,16 +210,17 @@ if len(SNPX1)!=0:
     MIDDLE=sum(SNPwindows)/float(len(SNPwindows))
     LENGTH=END-START
 
-    out.write(Chr+"\t"+"\t".join([*map(str,[START,MIDDLE,END,LENGTH])])+"\t"+str(len(SNPX1))+"\t"+D1s+"\t"+D2s+"\t"+D3s+"\n")
+    outWindow.write(Chr+"\t"+"\t".join([str(x) for x in [START,MIDDLE,END,LENGTH]])+"\t"+str(len(SNPX1))+"\t"+D1s+"\t"+D2s+"\n")
+
+
+## open Outputfile for genome-wide estimates
+outGW=open(options.OUT+"_GenomeWide.D","w")
 
 ### calculate Jacknife stat
-print("Method\tDest\tDjackEst\tDjackVar\tZ")
+outGW.write("Method\tDest\tDjackEst\tDjackVar\tZ\n")
 
 Z1=blockJackEven(fSNPX1,fSNPY1,D1L)
-print("SoraggiEtAl\t"+"\t".join([*map(str,Z1)]))
+outGW.write("SoraggiEtAl\t"+"\t".join([str(x) for x in Z1])+"\n")
 
 Z2=blockJackEven(fSNPX2,fSNPY2,D2L)
-print("DurandEtAl\t"+"\t".join([*map(str,Z2)]))
-
-Z3=blockJackEven(fSNPX3,fSNPY3,D3L)
-print("MartinEtAl\t"+"\t".join([*map(str,Z3)]))
+outGW.write("DurandEtAl\t"+"\t".join([str(x) for x in Z2])+"\n")
